b46700d1c893fd74bd9e608097a9e0dfe1a83b16
wangjiao <1727289689@qq.co>
2018-05-29 16:20:58 +0800

    push

--- app/src/main/java/voc/cn/cnvoccoin/adapter/BasicAdapter.kt
+++ app/src/main/java/voc/cn/cnvoccoin/adapter/BasicAdapter.kt
@@ -1,8 +1,11 @@
 package voc.cn.cnvoccoin.adapter
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,8 +15,6 @@ import voc.cn.cnvoccoin.activity.*
 import voc.cn.cnvoccoin.util.PreferenceUtil
 import voc.cn.cnvoccoin.util.TOKEN
 import voc.cn.cnvoccoin.util.ToastUtil
-import android.content.ClipboardManager;
-import android.content.ClipData
 import voc.cn.cnvoccoin.util.USER_ID
 
 class BasicAdapter(var mContext: Context, var data: ArrayList<Int>, var tag: Int) : RecyclerView.Adapter<BasicAdapter.ViewHolder>() {
@@ -36,18 +37,18 @@ class BasicAdapter(var mContext: Context, var data: ArrayList<Int>, var tag: Int
 //                            mContext.startActivity(Intent(mContext, InvitationActivity::class.java))
 
 
-                            val clipboardManager = mContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                            if (clipboardManager.hasPrimaryClip()) {
 
-                                val uid = PreferenceUtil.instance?.getString(USER_ID)
-                                if (uid != null ) {
-                                    clipboardManager.primaryClip = ClipData.newPlainText(null, "@string/copy_inviteText"+uid)
-                                }
-                            }
-                                clipboardManager.primaryClip!!.getItemAt(0).text
-                            }
+
+//                            //获取剪贴板管理器：
+                            val cm = mContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+//                            // 创建普通字符型ClipData
+                            val uid = PreferenceUtil.instance?.getInt(USER_ID)
+////                            val uid = "haha"
+                            Log.d("9999999999999","uuuuuu"+uid)
+                            val mClipData = ClipData.newPlainText("Label", "2018最落地，区块链+智能语音大数据场景应用。比特币挖矿模式，拒绝传销，拒绝虚拟矿机，社区成员多劳多得。个人语音就能挖矿，打造去中心全球公有语音大数据库。已和国内知名语音识别公司展开合作，市场千亿市值，6月28日上所。注册即送3000VOC，一枚价值0.1元，每天朗读15分钟，轻松获得2000币，价值200元。立刻下载：http://www.vochain.world/user/index/htmlregist.html?invite_code="+uid)
+                            // 将ClipData内容放到系统剪贴板里。
+                            cm.primaryClip = mClipData
                             ToastUtil.showToast("邀请链接已复制到剪贴板\n快去分享给你的朋友吧~")
-
                         }
                         1 -> {
                             mContext.startActivity(Intent(mContext, CommnutityActivity::class.java))
@@ -102,3 +103,8 @@ class BasicAdapter(var mContext: Context, var data: ArrayList<Int>, var tag: Int
 
 
 }
+
+
+
+
+
--- app/src/main/java/voc/cn/cnvoccoin/util/UrlConstants.kt
+++ app/src/main/java/voc/cn/cnvoccoin/util/UrlConstants.kt
@@ -13,3 +13,4 @@ const val UPLOAD_COIN = "/api/portal/voc/uploadVocCoinV2"//上传币
 const val MAKE_TASK = "/api/portal/voc/confirmCode"//确认任务
 const val TODAY_RANK = "/api/portal/voc/getTop"
 const val MOBILE_CONFIRM_CODE = "/api/user/public/sendCode"//获取手机验证码
+const val SHEQU_PICS = "/api/user/public/getCodeArr"//社区图片
