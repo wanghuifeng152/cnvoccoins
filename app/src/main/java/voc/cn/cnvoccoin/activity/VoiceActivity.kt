53fe4900bee447240ada01c553216fefcabb5c83
shihaiyan <15726676716@163.com>
2018-06-05 10:26:28 +0800

    录音保存

--- app/src/main/java/voc/cn/cnvoccoin/activity/VoiceActivity.kt
+++ app/src/main/java/voc/cn/cnvoccoin/activity/VoiceActivity.kt
@@ -1,8 +1,6 @@
 package voc.cn.cnvoccoin.activity
 
 import android.media.MediaRecorder
-import android.net.LocalServerSocket
-import android.net.LocalSocket
 import android.os.Build.VERSION_CODES.BASE
 import android.os.Bundle
 import android.view.MotionEvent
@@ -13,10 +11,10 @@ import voc.cn.cnvoccoin.network.HttpManager
 import voc.cn.cnvoccoin.network.RequestBodyWrapper
 import voc.cn.cnvoccoin.network.ResBaseModel
 import voc.cn.cnvoccoin.network.Subscriber
+import voc.cn.cnvoccoin.service.RecordingService.onRecord
 import voc.cn.cnvoccoin.util.ToastUtil
 import voc.cn.cnvoccoin.util.UPLOAD_COIN
 import voc.cn.cnvoccoin.util.UploadCoinRequest
-import java.io.*
 import java.math.BigDecimal
 import java.text.DecimalFormat
 
@@ -31,86 +29,54 @@ class VoiceActivity : BaseActivity() {
     var voice_coin: String = "0.00"
 
     private var mediarecorder: MediaRecorder? = null
-    private val sender: LocalSocket? = null
-    private val received: LocalSocket? = null
-    private val lss: LocalServerSocket? = null
-    private val BUFFER_SIZE = 5000
-    private val inputStream: InputStream? = null
-    private var recoder: Recoder? = null
     private var db: Double = 0.0  // 分贝
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         setContentView(R.layout.activity_voice)
         initView()
         getReadCoin()
-        val mRecorders = MediaRecorder()
-        mRecorders.setAudioSource(MediaRecorder.AudioSource.MIC)
-
-
-
     }
 
     private fun initView() {
 
-
         iv_voice?.setOnTouchListener { v, event ->
-            var startY:Float = 0f
+            var startY: Float = 0f
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     startY = event.y
                     oldTime = System.currentTimeMillis()
                     view_wave.startAnim()
-
-                    // 开始录音
-                    recoder = Recoder()
-                    recoder!!.start()
-
+                    onRecord(this, true)
                     true
                 }
                 MotionEvent.ACTION_UP -> {
 
                     //这是检测分贝的，不用就可以删除
                     mediarecorder = MediaRecorder()
-                    val ratio = mediarecorder!!. getMaxAmplitude () / BASE;
+                    val ratio = mediarecorder!!.getMaxAmplitude() / BASE;
                     db = 0.0;// 分贝
                     if (db < 1)
                         db = 20 * Math.log10(ratio.toDouble());
 
-                    // 结束录音
-                    if (mediarecorder != null) {
-
-                        //停止录音的
-                        mediarecorder.run {
-                            this!!.stop()
-                            release()
-                            mediarecorder = null
-                        }
-                    }
-
                     val endY = event.y
                     view_wave.stopAnim()
                     newTime = System.currentTimeMillis()
-
-                    if(endY -startY < -1000){
+                    if (endY - startY < -1000) {
                         ToastUtil.showToast("已取消")
-                    }else if (newTime - oldTime < 1000) {
+                    } else if (newTime - oldTime < 1000) {
                         //这里可以做人声判断
                         ToastUtil.showToast("录音时间过短")
                     }
-                    else if(db<-20){ // 检测音量
-                        ToastUtil.showToast("再大声一些哦~"+db)
-                    }
-                    else{
+                    /*    else if(db<-20){ // 检测音量
+                            ToastUtil.showToast("再大声一些哦~"+db)
+                        }*/
+                    else {
+                        onRecord(this, false)
                         getReadCoin()
                     }
                     true
-//                    else {
-//                    }
 
                 }
             }
@@ -120,6 +86,8 @@ class VoiceActivity : BaseActivity() {
     }
 
     private fun getReadCoin() {
+        /* val loadingDialog = LoadingDialog(this, "")
+         loadingDialog.show()*/
         val request = UploadCoinRequest(voice_id.toString())
         val wrapper = RequestBodyWrapper(request)
         HttpManager.post(UPLOAD_COIN, wrapper).subscribe(object : Subscriber<ResBaseModel<UploadVoiceBean>> {
@@ -141,59 +109,10 @@ class VoiceActivity : BaseActivity() {
             }
 
             override fun onComplete() {
+//                loadingDialog.dismiss()
             }
 
         }, UploadVoiceBean::class.java, ResBaseModel::class.java)
-    }
-
-    //文件转流
-    @Throws(Exception::class)
-    fun readStream(): ByteArray {
-        val fs = FileInputStream("/sdcard/today.aac")
-        val outStream = ByteArrayOutputStream()
-        val buffer = ByteArray(1024)
-        var len = fs.read(buffer)
-
-        while (-1 !=len) {
-            outStream.write(buffer, 0, len);
-        }
-
-        outStream.close()
-        fs.close()
-
-        return outStream.toByteArray()
-
-    }
-
-    //调用录音机，存储录音文件
-    internal inner class Recoder : Thread() {
-        override fun run() {
-            // TODO Auto-generated method stub
-            super.run()
-            val file = File("/sdcard/today.aac")
-            mediarecorder = MediaRecorder()
-            if (file.exists()) {
-                // 创建新的空文件
-                file.delete();
-            }
-            if (mediarecorder == null) {
-                mediarecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
-                mediarecorder!!.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
-                mediarecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-                mediarecorder!!.setOutputFile(file.absolutePath)
-            }
-            try {
-                mediarecorder!!.prepare()
-            } catch (e: IllegalStateException) {
-                // TODO Auto-generated catch block
-                e.printStackTrace()
-            } catch (e: IOException) {
-                // TODO Auto-generated catch block
-                e.printStackTrace()
-            }
-
-            mediarecorder!!.start()
-        }
     }
 
 }
---
+++ app/src/main/java/voc/cn/cnvoccoin/service/RecordingService.java
@@ -0,0 +1,109 @@
+package voc.cn.cnvoccoin.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+
+import voc.cn.cnvoccoin.R;
+
+/**
+ * Created by shy on 2018/6/5.
+ */
+
+public class RecordingService extends Service {
+    public String mFileName;
+    public String mFilePath;
+    MediaRecorder mediaRecorder;
+    Long mStartingTimeMillis;
+    long mElapsedMillis;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startRecording();
+        return START_STICKY;
+    }
+
+    private void startRecording() {
+        setFileNameAndPath();
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);//mp4
+        mediaRecorder.setOutputFile(mFilePath);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setAudioChannels(1);
+        // 设置录音文件的清晰度
+        mediaRecorder.setAudioSamplingRate(44100);
+        mediaRecorder.setAudioEncodingBitRate(192000);
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            mStartingTimeMillis = System.currentTimeMillis();
+        } catch (IOException e) {
+        }
+
+    }
+
+    // 停止录音
+    public void stopRecording() {
+        mediaRecorder.stop();
+        mElapsedMillis = (System.currentTimeMillis() - mStartingTimeMillis);
+        mediaRecorder.release();
+        getSharedPreferences("sp_name_audio", MODE_PRIVATE)
+                .edit()
+                .putString("audio_path", mFilePath)
+                .putLong("elpased", mElapsedMillis)
+                .apply();
+  /*      if (mIncrementTimerTask != null) {
+            mIncrementTimerTask.cancel();
+            mIncrementTimerTask = null;
+        }*/
+    }
+
+    // 设置录音文件的名字和保存路径
+    public void setFileNameAndPath() {
+        File f;
+        do {
+//            count++;
+            mFileName = getString(R.string.default_file_name)
+                    + "_" + (System.currentTimeMillis()) + ".mp4";
+            mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath += "/SoundRecorder/" + mFileName;
+            f = new File(mFilePath);
+        } while (f.exists() && !f.isDirectory());
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaRecorder != null) {
+            stopRecording();
+        }
+        super.onDestroy();
+    }
+
+    public static void onRecord(Context context,boolean start){
+        Intent intent = new Intent(context, RecordingService.class);
+        if (start) {
+            File folder = new File(Environment.getExternalStorageDirectory() + "/SoundRecorder");
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+            context.startService(intent);
+
+        } else {
+            context.stopService(intent);
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
