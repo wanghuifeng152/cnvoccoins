fd4d9ad2984274076a2a6347e97d333f1ce4fc8c
shihaiyan <15726676716@163.com>
2018-06-07 09:45:03 +0800

    update

--- app/src/main/java/voc/cn/cnvoccoin/activity/VoiceActivity.kt
+++ app/src/main/java/voc/cn/cnvoccoin/activity/VoiceActivity.kt
@@ -16,7 +16,6 @@ import voc.cn.cnvoccoin.network.RequestBodyWrapper
 import voc.cn.cnvoccoin.network.ResBaseModel
 import voc.cn.cnvoccoin.network.Subscriber
 import voc.cn.cnvoccoin.service.RecordingService
-import voc.cn.cnvoccoin.service.RecordingService.onRecord
 import voc.cn.cnvoccoin.util.ToastUtil
 import voc.cn.cnvoccoin.util.UPLOAD_COIN
 import voc.cn.cnvoccoin.util.UploadCoinRequest
@@ -32,13 +31,14 @@ class VoiceActivity : BaseActivity() {
     var newTime: Long = 0
     var voice_id: Int = 0
     var voice_coin: String = "0.00"
-
+    var service:RecordingService? = null
     private var mideaRecoder: MediaRecorder? = null
     private var db: Double = 0.0  // 分贝
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_voice)
+         service = RecordingService()
         initView()
         getReadCoin()
     }
@@ -53,99 +53,108 @@ class VoiceActivity : BaseActivity() {
                     startY = event.y
                     oldTime = System.currentTimeMillis()
                     view_wave.startAnim()
-                    onRecord(this, true, connection)
-                    Thread(mUpdateMicStatusTimer).start()
-                    true
-                }
-                MotionEvent.ACTION_UP -> {
-                    val endY = event.y
-                    view_wave.stopAnim()
-                    newTime = System.currentTimeMillis()
-                    if (endY - startY < -1000) {
-                        ToastUtil.showToast("已取消")
-                        onRecord(this, false, connection)
+                    service?.onRecord(this, true)
+                                        Thread(mUpdateMicStatusTimer).start()
+              /*          if ( RecordingService().mideaRecoder != null) {
+                            val ratio = mideaRecoder!!.maxAmplitude as Double
 
-                    } else if (newTime - oldTime < 1000) {
-                        //这里可以做人声判断
-                        ToastUtil.showToast("录音时间过短")
-                        onRecord(this, false, connection)
-                    } else {
-                        onRecord(this, false, connection)
-                        getReadCoin()
+                            Log.d("", "音量:ratio" + ratio)
+                            var db = 0.0// 分贝
+                            if (ratio > 1)
+                                db = 20 * Math.log10(ratio)
+                        }*/
+                        true
                     }
-                    true
+                    MotionEvent.ACTION_UP -> {
+                        val endY = event.y
+                        view_wave.stopAnim()
+                        newTime = System.currentTimeMillis()
+                        if (endY - startY < -1000) {
+                            ToastUtil.showToast("已取消")
+//                            onRecord(this, false, connection)
 
+                        } else if (newTime - oldTime < 1000) {
+                            //这里可以做人声判断
+                            ToastUtil.showToast("录音时间过短")
+//                            onRecord(this, false, connection)
+                        } else {
+                            service?.onRecord(this, false)
+                            getReadCoin()
+                        }
+                        true
+
+                    }
                 }
-            }
-            true
+                true
 
+            }
         }
-    }
 
-    private fun getReadCoin() {
-        /* val loadingDialog = LoadingDialog(this, "")
-         loadingDialog.show()*/
-        val request = UploadCoinRequest(voice_id.toString())
-        val wrapper = RequestBodyWrapper(request)
-        HttpManager.post(UPLOAD_COIN, wrapper).subscribe(object : Subscriber<ResBaseModel<UploadVoiceBean>> {
-            override fun onNext(model: ResBaseModel<UploadVoiceBean>?) {
-                if (model?.data == null || model?.data.next == null) return
-                if (model.code != 1) return
-                tv_voice_text.text = model.data.next.content
-                if (voice_id != 0) {
-                    val bigDecimal = BigDecimal(voice_coin)
-                    val plus = BigDecimal(model.data.next.voc_coin).plus(bigDecimal)
-                    val format = DecimalFormat("#######.##")
-                    voice_coin = format.format(plus)
+        private fun getReadCoin() {
+            /* val loadingDialog = LoadingDialog(this, "")
+             loadingDialog.show()*/
+            val request = UploadCoinRequest(voice_id.toString())
+            val wrapper = RequestBodyWrapper(request)
+            HttpManager.post(UPLOAD_COIN, wrapper).subscribe(object : Subscriber<ResBaseModel<UploadVoiceBean>> {
+                override fun onNext(model: ResBaseModel<UploadVoiceBean>?) {
+                    if (model?.data == null || model?.data.next == null) return
+                    if (model.code != 1) return
+                    tv_voice_text.text = model.data.next.content
+                    if (voice_id != 0) {
+                        val bigDecimal = BigDecimal(voice_coin)
+                        val plus = BigDecimal(model.data.next.voc_coin).plus(bigDecimal)
+                        val format = DecimalFormat("#######.##")
+                        voice_coin = format.format(plus)
+                    }
+                    voice_id = model.data.next.id
+                    tv_have_coin.text = voice_coin
                 }
-                voice_id = model.data.next.id
-                tv_have_coin.text = voice_coin
-            }
 
-            override fun onError(t: Throwable?) {
-            }
+                override fun onError(t: Throwable?) {
+                }
 
-            override fun onComplete() {
+                override fun onComplete() {
 //                loadingDialog.dismiss()
+                }
+
+            }, UploadVoiceBean::class.java, ResBaseModel::class.java)
+        }
+
+     /*   private var connection: ServiceConnection = object : ServiceConnection {
+            override fun onServiceDisconnected(name: ComponentName) {
+                ToastUtil.showToast("fail")
             }
 
-        }, UploadVoiceBean::class.java, ResBaseModel::class.java)
-    }
+            override fun onServiceConnected(name: ComponentName, service: IBinder) {
+                ToastUtil.showToast("success")
+                val binder = service as RecordingService.MyBinder
+                var recordingService = binder.service as RecordingService
+                mideaRecoder = recordingService.mideaRecoder
+            }
+        }*/
 
-    private var connection: ServiceConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName) {
-            ToastUtil.showToast("fail")
+        private val mHandler = Handler()
+        private val mUpdateMicStatusTimer = Runnable { updateMicStatus() }
+
+
+        /**
+         * 更新话筒状态
+         *
+         */
+        private val SPACE: Long = 100// 间隔取样时间
+
+        private fun updateMicStatus() {
+            if (service?.mideaRecoder != null) {
+                val ratio = service?.mideaRecoder!!.maxAmplitude
+
+                Log.d("", "音量:ratio" + ratio)
+        /*        var db = 0.0// 分贝
+                if (ratio > 1)
+                    db = 20 * Math.log10(ratio.toDouble())
+                Log.d("", "音量：$db")*/
+                mHandler.postDelayed(mUpdateMicStatusTimer, SPACE)
+            }
         }
 
-        override fun onServiceConnected(name: ComponentName, service: IBinder) {
-            ToastUtil.showToast("success")
-            val binder = service as RecordingService.MyBinder
-            var recordingService = binder.service as RecordingService
-            mideaRecoder = recordingService.mideaRecoder
-        }
+
     }
-
-    private val mHandler = Handler()
-    private val mUpdateMicStatusTimer = Runnable { updateMicStatus() }
-
-
-    /**
-     * 更新话筒状态
-     *
-     */
-    private val SPACE: Long = 100// 间隔取样时间
-
-    private fun updateMicStatus() {
-        if (mideaRecoder != null) {
-            val ratio = mideaRecoder!!.maxAmplitude as Double
-
-            Log.d("", "音量:ratio" + ratio)
-            var db = 0.0// 分贝
-            if (ratio > 1)
-                db = 20 * Math.log10(ratio)
-            Log.d("", "音量：$db")
-            mHandler.postDelayed(mUpdateMicStatusTimer, SPACE)
-        }
-    }
-
-}
--- app/src/main/java/voc/cn/cnvoccoin/service/RecordingService.java
+++ app/src/main/java/voc/cn/cnvoccoin/service/RecordingService.java
@@ -19,7 +19,7 @@ import voc.cn.cnvoccoin.R;
  * Created by shy on 2018/6/5.
  */
 
-public class RecordingService extends Service {
+public class RecordingService  {
     public String mFileName;
     public String mFilePath;
     MediaRecorder mediaRecorder;
@@ -27,32 +27,34 @@ public class RecordingService extends Service {
     long mElapsedMillis;
     File f;
     //通过binder实现了 调用者（client）与 service之间的通信
-    private MyBinder binder = new MyBinder();
+//    private MyBinder binder = new MyBinder();
 
-    @Override
+/*    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         startRecording();
         return START_STICKY;
-    }
+    }*/
 
     private void startRecording() {
         setFileNameAndPath();
         if(mediaRecorder == null){
             mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);//mp4
+            mediaRecorder.setOutputFile(mFilePath);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            mediaRecorder.setAudioChannels(1);
+            // 设置录音文件的清晰度
+            mediaRecorder.setAudioSamplingRate(44100);
+            mediaRecorder.setAudioEncodingBitRate(192000);
         }
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);//mp4
-        mediaRecorder.setOutputFile(mFilePath);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        mediaRecorder.setAudioChannels(1);
-        // 设置录音文件的清晰度
-        mediaRecorder.setAudioSamplingRate(44100);
-        mediaRecorder.setAudioEncodingBitRate(192000);
+
         try {
             mediaRecorder.prepare();
             mediaRecorder.start();
             mStartingTimeMillis = System.currentTimeMillis();
         } catch (IOException e) {
+        }catch (IllegalStateException e){
         }
 
     }
@@ -75,8 +77,8 @@ public class RecordingService extends Service {
 
     // 设置录音文件的名字和保存路径
     public void setFileNameAndPath() {
-        mFileName = getString(R.string.default_file_name)
-                + "_" + ".mp4";
+        mFileName = /*getString(R.string.default_file_name)
+                + */"_" + ".mp4";
         mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFilePath += "/SoundRecorder/" + mFileName;
         f = new File(mFilePath);
@@ -85,42 +87,44 @@ public class RecordingService extends Service {
         }
     }
 
-    @Override
+/*    @Override
     public void onDestroy() {
         if (mediaRecorder != null) {
             stopRecording();
         }
         super.onDestroy();
-    }
+    }*/
 
-    public static void onRecord(Context context, boolean start, ServiceConnection connection) {
+    public  void onRecord(Context context, boolean start) {
         Intent intent = new Intent(context, RecordingService.class);
         if (start) {
             File folder = new File(Environment.getExternalStorageDirectory() + "/SoundRecorder");
             if (!folder.exists()) {
                 folder.mkdir();
             }
-            context.startService(intent);
-            context.bindService(intent, connection, BIND_AUTO_CREATE);
+       /*     context.startService(intent);
+            context.bindService(intent, connection, BIND_AUTO_CREATE);*/
+            startRecording();
 
         } else {
-            context.stopService(intent);
-            context.unbindService(connection);
+       /*     context.stopService(intent);
+            context.unbindService(connection);*/
+       stopRecording();
         }
     }
 
 
-    @Nullable
+/*    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
-    }
+    }*/
 
-    public class MyBinder extends Binder{
+/*    public class MyBinder extends Binder{
         public RecordingService getService(){
             return RecordingService.this;
         }
-    }
+    }*/
 }
 
 
