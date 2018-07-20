3167b19782a7d096fa0fe3a4e6ff957f4d85b4f3
liujinhan <1377593988@qq.com>
2018-07-20 18:34:06 +0800

    添加数美设备so库

--- app/src/main/java/voc/cn/cnvoccoin/network/HttpConfig.java
+++ app/src/main/java/voc/cn/cnvoccoin/network/HttpConfig.java
@@ -2,7 +2,7 @@ package voc.cn.cnvoccoin.network;
 public class HttpConfig {
 
     public static final int DEFAULT_TIMEOUT_SECONDS = 15;
-    //public static final String DEFAULT_HOST = "http://172.11.20.127/voc/public/";
+//    public static final String DEFAULT_HOST = "http://dev.vochain.world";
     public static final String DEFAULT_HOST = "http://www.vochain.world";
 
 }
