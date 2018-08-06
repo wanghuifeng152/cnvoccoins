3a4ba4b7c259f752f50e8c2e5f5b3761a802976d
liujinhan <1377593988@qq.com>
2018-08-06 19:16:59 +0800

    设置界面添加重置密码

--- app/src/main/java/voc/cn/cnvoccoin/activity/SettingActivity.java
+++ app/src/main/java/voc/cn/cnvoccoin/activity/SettingActivity.java
@@ -1,5 +1,6 @@
 package voc.cn.cnvoccoin.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,7 +15,17 @@ import org.json.JSONObject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import voc.cn.cnvoccoin.R;
+import voc.cn.cnvoccoin.VocApplication;
+import voc.cn.cnvoccoin.network.HttpManager;
+import voc.cn.cnvoccoin.network.RequestBodyWrapper;
+import voc.cn.cnvoccoin.network.Subscriber;
 import voc.cn.cnvoccoin.util.AppUtils;
+import voc.cn.cnvoccoin.util.PreferenceUtil;
+import voc.cn.cnvoccoin.util.ResetPwd2;
+import voc.cn.cnvoccoin.util.ToastUtil;
+import voc.cn.cnvoccoin.util.postId;
+
+import static voc.cn.cnvoccoin.util.UrlConstantsKt.POST_IS_HAVE_PWD;
 
 /**
  *
@@ -26,7 +37,8 @@ public class SettingActivity extends BaseActivity {
     RelativeLayout llTitle;
     @BindView(R.id.tv_version)
     TextView tvVersion;
-
+    @BindView(R.id.rl_reset_pwd)
+    RelativeLayout rl_reset_pwd;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,7 +66,58 @@ public class SettingActivity extends BaseActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        rl_reset_pwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postIsHavePwd();
+            }
+        });
     }
 
 
+            private void postIsHavePwd(){
+                postId request = new postId("11");
+                RequestBodyWrapper wrapper = new RequestBodyWrapper(request);
+                HttpManager.post(POST_IS_HAVE_PWD, wrapper).subscribe(new Subscriber<String>(){
+
+
+                    @Override
+                    public void onNext(String s) {
+                        if (s == null || s.isEmpty()) return;
+                        JSONObject jsonObject = null;
+                        try {
+                            jsonObject = new JSONObject(s);
+                            int code = jsonObject.getInt("code");
+                            if (code == 1) {
+                                if (jsonObject.getString("msg").equals("还没有支付密码")){
+                                    ToastUtil.showToast("您还没有设置支付密码, 请先设置支付密码");
+                                    VocApplication.Companion.getInstance().setMessage_flag(true);
+                                    Intent intent = new Intent(SettingActivity.this,MessageCodeActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    //有设置密码去重置
+                                    VocApplication.Companion.getInstance().setResetPwd(true);
+                                    PreferenceUtil.Companion.getInstance().set("pwdFlag", "1");//首页重置密码跳转
+                                    Intent intent = new Intent(SettingActivity.this,GetMessageCodeActivity.class);
+                                    intent.putExtra("isTitle",0);
+                                    startActivity(intent);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+            }
 }
